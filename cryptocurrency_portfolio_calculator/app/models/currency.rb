6900f7ec6120432cb5b3dcff3d4208d9cb114394
CS-Skypper <skypper@localhost.localdomain>
2020-03-10 22:26:43 +0100

    currencies MVC

--- cryptocurrency_portfolio_calculator/app/models/currency.rb
+++ cryptocurrency_portfolio_calculator/app/models/currency.rb
@@ -1,2 +1,14 @@
 class Currency < ApplicationRecord
+    def calculate_value(amount)
+        (current_price.to_f * amount.to_f).round(4)
+    
+    end
+
+
+    def current_price
+        url = 'https://api.coinmarketcap.com/v1/ticker/'
+        # url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?PRO_API_KEY=48e961af-b28f-402e-b44a-8b67300dbbd8'
+        request = HTTParty.get(url + self.slug)
+        response = JSON.parse(request.body)[0]["price_usd"]
+    end
 end
